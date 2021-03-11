0ea76038193c547103d290d13bcc3fd3c45592d2
TUdby <74376246+TUdby@users.noreply.github.com>
2021-03-10 22:24:44 -0800

    Set toast messages, customize toast

--- app/src/main/java/com/example/flashcard/AddCardActivity.java
+++ app/src/main/java/com/example/flashcard/AddCardActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -22,13 +26,39 @@ public class AddCardActivity extends AppCompatActivity {
         findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String question = ((EditText) findViewById(R.id.question_field)).getText().toString();
-                String answer = ((EditText) findViewById(R.id.answer_field)).getText().toString();
-                Intent data = new Intent(AddCardActivity.this, MainActivity.class);
-                data.putExtra("question", question);
-                data.putExtra("answer", answer);
-                setResult(RESULT_OK, data);
-                finish();
+                // Inflate toast XML layout
+                View layout = getLayoutInflater().inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_layout_root));
+                TextView text = (TextView) layout.findViewById(R.id.text);
+
+                String q = ((EditText) findViewById(R.id.question_field)).getText().toString();
+                String a = ((EditText) findViewById(R.id.answer_field)).getText().toString();
+                if(q.equals("") && a.equals("")) {
+                    text.setText("Both fields must be filled");
+                    Toast toast = new Toast(getApplicationContext());
+                    toast.setView(layout);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.show();
+                } else if(q.equals("")) {
+                    text.setText("Question field must be filled");
+                    Toast toast = new Toast(getApplicationContext());
+                    toast.setView(layout);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.show();
+                } else if(a.equals("")) {
+                    text.setText("Answer field must be filled");
+                    Toast toast = new Toast(getApplicationContext());
+                    toast.setView(layout);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.show();
+                } else {
+                    String question = ((EditText) findViewById(R.id.question_field)).getText().toString();
+                    String answer = ((EditText) findViewById(R.id.answer_field)).getText().toString();
+                    Intent data = new Intent(AddCardActivity.this, MainActivity.class);
+                    data.putExtra("question", question);
+                    data.putExtra("answer", answer);
+                    setResult(RESULT_OK, data);
+                    finish();
+                }
             }
         });
 
